ceecebb896226c1032a2bdf6785b193a96e22f91
shaneorourke <shaneorourke110@gmail.com>
2022-10-22 11:52:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
-PATH=$(dirname "$0")
-
-cd $PATH &&
-python -m venv env &&
+chmod +x sma_cross.sh &&
+chmod +x sma_cross_while_loop.sh &&
+mkdir /home/shane/Documents/cronlogs -p &&
+python3 -m venv env &&
 source env/bin/activate &&
 pip install -r requirements.txt &&
 deactivate
