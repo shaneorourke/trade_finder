e032082451bcf86b0722512badd486aa0a12560e
shaneorourke <shaneorourke110@gmail.com>
2022-11-06 16:07:50 +0000

    Update trade_finder_lite.sh

--- trade_finder_lite.sh
+++ trade_finder_lite.sh
@@ -3,5 +3,9 @@
 PATH=$(dirname "$0")
 
 cd $PATH &&
-$(clear >&2) &&
-sqlite3 trade.db 'select symbol, exchange, screener, interval, status, ema_cross from symbol_stats'
+while 1=1
+do
+    /usr/bin/clear &&
+    /usr/bin/sqlite3 trade.db 'select symbol, exchange, screener, interval, status, ema_cross from symbol_stats'
+    sleep 30
+done
