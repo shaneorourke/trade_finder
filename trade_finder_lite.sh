6d5762a1787fe2ff8e1f84e1c55ee9d1b5699524
shaneorourke <shaneorourke110@gmail.com>
2022-11-06 16:14:58 +0000

    Update trade_finder_lite.sh

--- trade_finder_lite.sh
+++ trade_finder_lite.sh
@@ -6,6 +6,6 @@ cd $PATH &&
 while [ $x -le 5 ]
 do
     /usr/bin/clear &&
-    /usr/bin/sqlite3 trade.db 'select symbol, exchange, screener, interval, status, ema_cross from symbol_stats'
+    /usr/bin/sqlite3 trade.db 'select symbol, exchange, screener, interval, status, ema_cross from symbol_stats where status != "waiting" or ema_cross != "%waiting%"'
     /usr/bin/sleep 30
 done
